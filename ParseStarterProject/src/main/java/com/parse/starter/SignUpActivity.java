e0491e9731471a25077444dc1fb5aecacb6c9f1b
Nick Wonge <nkwong96@gmail.com>
2016-01-30 21:33:52 -0800

    Fixed buttons and some layers

--- ParseStarterProject/src/main/java/com/parse/starter/SignUpActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/SignUpActivity.java
@@ -32,7 +32,7 @@ public class SignUpActivity extends ActionBarActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        supportRequestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.sign_up);
 
 
