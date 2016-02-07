4d4df3c871eb4f97d8f25f78c7745eb90fff4751
Nick Wonge <nkwong96@gmail.com>
2016-02-07 02:17:21 -0800

    Added remove class option to pre-profile page

--- ParseStarterProject/src/main/java/com/parse/starter/PreProfileActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/PreProfileActivity.java
@@ -1,6 +1,7 @@
 package com.parse.starter;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
@@ -31,8 +33,10 @@ public class PreProfileActivity extends AppCompatActivity {
     private int MAX_CLASSES = 5;
     EditText[] classes = new EditText[MAX_CLASSES];
     Button addClassButton;
+    Button[] removeClasses = new Button[MAX_CLASSES];
     Button submitButton;
     int currentClass = 0;
+    int removeNum = 0;
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -46,26 +50,29 @@ public class PreProfileActivity extends AppCompatActivity {
 
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);        addClassButton = (Button) findViewById(R.id.AddClass);
         classes[0] = (EditText) findViewById(R.id.Class1);
+        removeClasses[0] = (Button) findViewById(R.id.RemoveClass1);
+        removeClasses[0].setVisibility(View.GONE);
         for (int i = 1; i < MAX_CLASSES; i++) {
             switch (i) {
                 case (1):
                     classes[i] = (EditText) findViewById(R.id.Class2);
-                    classes[i].setVisibility(View.GONE);
+                    removeClasses[i] = (Button) findViewById(R.id.RemoveClass2);
                     break;
                 case (2):
                     classes[i] = (EditText) findViewById(R.id.Class3);
-                    classes[i].setVisibility(View.GONE);
+                    removeClasses[i] = (Button) findViewById(R.id.RemoveClass3);
                     break;
                 case (3):
                     classes[i] = (EditText) findViewById(R.id.Class4);
-                    classes[i].setVisibility(View.GONE);
+                    removeClasses[i] = (Button) findViewById(R.id.RemoveClass4);
                     break;
                 case (4):
                     classes[i] = (EditText) findViewById(R.id.Class5);
-                    classes[i].setVisibility(View.GONE);
-
+                    removeClasses[i] = (Button) findViewById(R.id.RemoveClass5);
                     break;
             }
+            classes[i].setVisibility(View.GONE);
+            removeClasses[i].setVisibility(View.GONE);
         }
 
         submitButton = (Button) findViewById(R.id.submitbutton);
@@ -98,24 +105,60 @@ public class PreProfileActivity extends AppCompatActivity {
         }
         catch(IOException e) {}*/
 
+        for(int i = 0; i < MAX_CLASSES; i++) {
+            final Button removeButton = removeClasses[i];
+            removeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    for(int j = 0; j < MAX_CLASSES; j++) {
+                        if(removeClasses[j].getId() == v.getId()) {
+                            for (int k = j; k < currentClass; k++) {
+                                classes[k].setText(classes[k + 1].getText());
+                                classes[k + 1].setText("");
+                            }
+                            if (addClassButton.getVisibility() == View.INVISIBLE) {
+                                addClassButton.setVisibility(View.VISIBLE);
+                                removeClasses[currentClass].setVisibility(View.INVISIBLE);
+                            }
+                            else {
+                                removeClasses[currentClass - 1].setVisibility(View.INVISIBLE);
+                                classes[currentClass].setVisibility(View.INVISIBLE);
+                                currentClass--;
+                                classes[currentClass].requestFocus();
+                            }
+                            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)
+                                    addClassButton.getLayoutParams();
+                            params.addRule(RelativeLayout.ALIGN_BOTTOM, classes[currentClass].getId());
+                            addClassButton.setLayoutParams(params);
+                        }
+                    }
+                }
+            });
+        }
 
         addClassButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (currentClass == (MAX_CLASSES - 1)) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(PreProfileActivity.this);
-                    builder.setMessage("Stahp. No more classes pls.")
-                            .setTitle(R.string.login_error_title)
-                            .setPositiveButton(android.R.string.ok, null);
+                if(classes[currentClass].getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "bruh its empty", Toast.LENGTH_LONG).show();
                 }
                 else {
-                    currentClass++;
-                    classes[currentClass].setVisibility(View.VISIBLE);
-                    classes[currentClass].requestFocus();
-                    RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)
-                            addClassButton.getLayoutParams();
-                    params.addRule(RelativeLayout.ALIGN_BOTTOM, classes[currentClass].getId());
-                    addClassButton.setLayoutParams(params);
+                    removeClasses[currentClass].setVisibility(View.VISIBLE);
+                    if (currentClass == (MAX_CLASSES - 1)) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(PreProfileActivity.this);
+                        builder.setMessage("Stahp. No more classes pls.")
+                                .setTitle(R.string.login_error_title)
+                                .setPositiveButton(android.R.string.ok, null);
+                        addClassButton.setVisibility(View.INVISIBLE);
+                    } else {
+                        currentClass++;
+                        classes[currentClass].setVisibility(View.VISIBLE);
+                        classes[currentClass].requestFocus();
+                        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)
+                                addClassButton.getLayoutParams();
+                        params.addRule(RelativeLayout.ALIGN_BOTTOM, classes[currentClass].getId());
+                        addClassButton.setLayoutParams(params);
+                    }
                 }
             }
         });
