23b1b92fdc4cc0d3a646af20fb6e92c7c81fbb84
Nick Wonge <nkwong96@gmail.com>
2016-02-22 16:44:46 -0800

    Added upload from gallery feature.

--- ParseStarterProject/src/main/java/com/parse/starter/PreProfileActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/PreProfileActivity.java
@@ -4,8 +4,12 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -16,6 +20,7 @@ import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
@@ -30,6 +35,7 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -49,6 +55,8 @@ public class PreProfileActivity extends AppCompatActivity {
     int passedInCurrentClass;
     ParseUser user;
     ParseObject profile;
+    ImageView profilePic;
+    int SELECT_PHOTO = 1;
 
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
@@ -187,22 +195,6 @@ public class PreProfileActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        /*try {
-            File f = new File("profilepic");
-            InputStream is = getResources().openRawResource(R.drawable.exam);
-            OutputStream out = new FileOutputStream(f);
-            byte buf[] = new byte[1024];
-            int len;
-            while ((len = is.read(buf)) > 0) {
-                out.write(buf, 0, len);
-                out.close();
-                is.close();
-            }
-            byte[] data = f.getAbsolutePath().getBytes();
-            ParseFile file = new ParseFile("img1.jpg",data);
-            file.saveInBackground();
-        }
-        catch(IOException e) {}*/
 
         for(int i = 0; i < MAX_CLASSES; i++) {
             final Button removeButton = removeClasses[i];
@@ -263,9 +255,58 @@ public class PreProfileActivity extends AppCompatActivity {
                 }
             }
         });
+
+        profilePic = (ImageView) findViewById(R.id.profileImage);
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+            }
+        });
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Here we need to check if the activity that was triggers was the Image Gallery.
+        // If it is the requestCode will match the LOAD_IMAGE_RESULTS value.
+        // If the resultCode is RESULT_OK and there is some data we know that an image was picked.
+        if (requestCode == SELECT_PHOTO && resultCode == RESULT_OK && data != null) {
+            // Let's read picked image data - its URI
+            Uri pickedImage = data.getData();
+            // Let's read picked image path using content resolver
+            String[] filePath = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(pickedImage, filePath, null, null, null);
+            cursor.moveToFirst();
+            String imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
+            cursor.close();
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath, options);
+            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+            // Compress image to lower quality scale 1 - 100
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+            byte[] image = stream.toByteArray();
+
+            // Create the ParseFile
+            ParseFile file = new ParseFile("profpic.png", image);
+            // Upload the image into Parse Cloud
+            file.saveInBackground();
+
+            user.put("ProfPic", file);
+
+            // Create the class and the columns
+            user.saveInBackground();
+
+            // Show a simple toast message
+            Toast.makeText(getApplicationContext(), "Image Uploaded", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void addProfileContent(ParseObject profile, String name, ParseUser user){
