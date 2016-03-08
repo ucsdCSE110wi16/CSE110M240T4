7d56ae234c38146874d53791d4f172609e30b5ef
Jarnin Fang <jarninfang@gmail.com>
2016-03-08 12:22:48 -0800

    Test if clicking on profile picture opens up the gallery

--- ParseStarterProject/src/androidTest/java/com/parse/starter/PreProfileTest.java
+++ ParseStarterProject/src/androidTest/java/com/parse/starter/PreProfileTest.java
@@ -1,6 +1,7 @@
 package com.parse.starter;
 
 import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
@@ -13,6 +14,8 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -33,5 +36,15 @@ public class PreProfileTest {
         onView(withId(R.id.AddClass)).perform(click());
         onView(withId(R.id.Class2)).check(matches(isDisplayed()));*/
         onView(withId(R.id.AddClass)).check(matches(isDisplayed()));
+    }
+
+    /*
+       TEST IF CLICKING THE PROFILE PICTURE IMAGE OPENS UP THE GALLERY
+     */
+    @Test
+    public void checkProfilePicture() {
+        Intents.init();
+        onView(withId(R.id.profileImage)).perform(click());
+        intended(toPackage("com.android.gallery"));
     }
 }
