190fdeae5783a466db431c7cf6c81b037321b285
Jarnin Fang <jarninfang@gmail.com>
2016-03-08 12:15:59 -0800

    Added successful login test

---
+++ ParseStarterProject/src/androidTest/java/com/parse/starter/LoginTest.java
@@ -0,0 +1,43 @@
+package com.parse.starter;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by jarnin on 3/8/16.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class LoginTest {
+    @Rule
+    public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule(LoginActivity.class);
+
+    @Test
+    public void tryLoggingIn() {
+        onView(withId(R.id.emailField)).perform(click()).perform(typeText("abcd14@ucsd.edu"));
+        onView(withId(R.id.emailField)).perform(closeSoftKeyboard());
+        onView(withId(R.id.passwordField)).perform(click()).perform(typeText("abcd"));
+        onView(withId(R.id.passwordField)).perform(closeSoftKeyboard());
+        onView(withId(R.id.loginButton)).perform(click());
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withId(R.id.profileImage)).check(matches(isDisplayed()));
+    }
+}
