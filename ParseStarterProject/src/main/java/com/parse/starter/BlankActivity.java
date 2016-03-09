20957814157cd9d0f2fb033a453bcd4188311015
Nick Wonge <nkwong96@gmail.com>
2016-03-09 02:41:29 -0800

    Added message button and logout to BlankActivity Removed unused files

--- ParseStarterProject/src/main/java/com/parse/starter/BlankActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/BlankActivity.java
@@ -4,13 +4,21 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import java.util.List;
 
 /**
  * Created by Ken on 2/8/2016.
@@ -19,6 +27,7 @@ public class BlankActivity extends AppCompatActivity {
 
     private TextView blankText;
     protected ImageView backImage;
+    private Button matchPageButton;
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -31,6 +40,24 @@ public class BlankActivity extends AppCompatActivity {
         setContentView(R.layout.blank_screen);
 
         blankText = (TextView) findViewById(R.id.textView4);
+
+        matchPageButton = (Button) findViewById(R.id.matchedPageButton);
+        matchPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseUser user = ParseUser.getCurrentUser();
+                List<ParseObject> file = (List<ParseObject>) user.get("MatchedProfiles");
+                if(file == null) {
+                    Toast.makeText(getApplicationContext(), "No matches!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    final Intent intent = new Intent(getApplicationContext(), MatchedPortfolio.class);
+                    final Intent serviceIntent = new Intent(getApplicationContext(), MessageService.class);
+                    startActivity(intent);
+                    startService(serviceIntent);
+                }
+            }
+        });
         backImage = (ImageView) findViewById(R.id.backToProfile);
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -87,4 +114,30 @@ public class BlankActivity extends AppCompatActivity {
         client.disconnect();
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_logout) {
+            stopService(new Intent(getApplicationContext(), MessageService.class));
+            ParseUser.logOut();
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
